95ceae37fabb72719594f26de923c86edec231ad
Joshua Zhang <cheungchih@gmail.com>
2014-09-05 16:08:07 -0500

    Initial commit
    Baseline framework

---
+++ src/define.h
@@ -0,0 +1,46 @@
+/***********************************************************************/
+/*                                                                    
+/*   Script File: define.h                                                          
+/*                        
+/*   Description:
+/*                                               
+/*   Definitions and typedef      
+/*   							               
+/*                                                                     
+/*   Author: Joshua Zhang                                              
+/*   Date:  Sep-2014                                                           
+/*                                                                     
+/*   Copyright(C) 2014  Joshua Zhang	 - All Rights Reserved.        
+/*                                                                     
+/*   This software is available for non-commercial use only. It must   
+/*   not be modified and distributed without prior permission of the   
+/*   author. The author is not responsible for implications from the   
+/*   use of this software.                                             
+/*                                                                     
+/***********************************************************************/
+
+
+
+#ifndef __DEFINE_H__
+#define __DEFINE_H__
+
+
+/*-------------Do not change between dashed lines----------------*/
+
+//macro
+#define		Malloc(type, size)	(type*)malloc((size)*sizeof(type))
+
+// typedef
+typedef unsigned int			UInt;
+typedef	unsigned char			UChar;
+typedef unsigned long			ULong;
+typedef	double					Double;
+typedef bool					Bool;
+
+/*-------------Do not change between dashed lines----------------*/
+
+
+
+/****************************************************************/
+
+#endif // _DEFINE_H_
---
+++ src/global.h
@@ -0,0 +1,47 @@
+/***********************************************************************/
+/*                                                                    
+/*   Script File: global.h                                                          
+/*                        
+/*   Description:
+/*                                               
+/*   Definition of global structures for  person re-identification     
+/*   							               
+/*                                                                     
+/*   Author: Joshua Zhang                                              
+/*   Date:  Sep-2014                                                           
+/*                                                                     
+/*   Copyright(C) 2014  Joshua Zhang	 - All Rights Reserved.        
+/*                                                                     
+/*   This software is available for non-commercial use only. It must   
+/*   not be modified and distributed without prior permission of the   
+/*   author. The author is not responsible for implications from the   
+/*   use of this software.                                             
+/*                                                                     
+/***********************************************************************/
+
+
+
+#ifndef __GLOBAL_H__
+#define __GLOBAL_H__
+
+#include "define.h"
+#include <vector>
+extern "C"{
+#include <vl/gmm.h>
+#include <vl/fisher.h>
+}
+
+using namespace std;
+
+
+//global structure 
+typedef struct HRI_struct
+{
+	
+
+}hrist;
+
+
+#endif //__GLOBAL_H__
+
+
